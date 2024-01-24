1fbb1fd2f4db97936716fdb263529e19ccef82d2
Bogachevv <dog67688@gmail.com>
2024-01-24 16:03:25 +0300

    Writing implementation, based of fastKde

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pva import plot_precision_curve
 
 
 def main():
-    X, y = make_regression(n_samples=1_000, n_features=1, noise=2, random_state=42)
+    X, y = make_regression(n_samples=100_000, n_features=1, noise=2, random_state=42)
 
     if np.min(y) < 0:
         y -= np.min(y) - 10
@@ -20,9 +20,17 @@ def main():
     ridge.fit(X_train, y_train)
     y_pred = ridge.predict(X_test)
 
+    y_pred += 100
+
     print(ridge.coef_)
 
-    plot_precision_curve(y_true=y_test, y_pred=y_pred, plot_optimal=True, rug_plot=True, plot_mode='relative')
+    _, ax = plt.subplots(ncols=2, sharey=True, sharex=True, figsize=(14, 5))
+
+    # plot_precision_curve(y_true=y_test, y_pred=y_pred, plot_optimal=True, rug_plot=True, show_legend=False,
+    #                      plot_mode='deviation', strategy='kde', ax=ax[0])
+
+    plot_precision_curve(y_true=y_test, y_pred=y_pred, plot_optimal=True, rug_plot=True, show_legend=False,
+                         plot_mode='deviation', strategy='fastkde', ax=ax[1])
 
     plt.show()
 
--- pva/src.py
+++ pva/src.py
@@ -6,6 +6,7 @@ from typing import List, Dict, Callable, Tuple, Union, Literal
 import warnings
 from bisect import bisect_left
 import itertools
+from fastkde import fastKDE
 
 
 def _check_quantiles_input(x, y, quantiles: List[float]):
@@ -43,7 +44,8 @@ def _compute_conditional_cdf(x_val, y_min: float, y_max: float, min_dots_cnt: in
 
     cdf = _compute_cdf(y, pdf_sp)
 
-    for p_cnt in np.logspace(min_dots_cnt+1, max_dots_cnt+1, num=(max_dots_cnt - min_dots_cnt + 1), dtype=int, base=2):
+    for p_cnt in np.logspace(min_dots_cnt + 1, max_dots_cnt + 1, num=(max_dots_cnt - min_dots_cnt + 1), dtype=int,
+                             base=2):
         cdf_old = cdf
 
         pdf_sp_old = pdf_sp
@@ -70,13 +72,13 @@ def _compute_conditional_cdf(x_val, y_min: float, y_max: float, min_dots_cnt: in
     return cdf, y
 
 
-def _qet_quantiles(x: Union[np.ndarray, Tuple[float, float]],
-                   y: Union[np.ndarray, Tuple[float, float]],
-                   pdf: Callable, quantiles: List[float],
-                   *,
-                   num_x_dots: int = 2 ** 8,
-                   num_y_dots: int = 2 ** 16,
-                   ) -> Dict[float, np.ndarray]:
+def _qet_quantiles_kde(x: Union[np.ndarray, Tuple[float, float]],
+                       y: Union[np.ndarray, Tuple[float, float]],
+                       pdf: Callable, quantiles: List[float],
+                       *,
+                       num_x_dots: int = 2 ** 8,
+                       num_y_dots: int = 2 ** 16,
+                       ) -> Dict[float, np.ndarray]:
     """
     :param x: If x is a numpy array, compute the quantiles for all elements in the array.
     Else compute the quantiles by np.linspace(x[0], x[1], num_x_dots)
@@ -104,14 +106,41 @@ def _qet_quantiles(x: Union[np.ndarray, Tuple[float, float]],
     return q_res
 
 
+def _compute_conditional_cdf_fastkde(x_val: float, fastkde):
+    pos = bisect_left(fastkde.coords['var0'], x_val)  # get position of x_val in x axis array, given by fastkde
+    assert x_val == fastkde.coords['var0'][pos], \
+        f"{x_val=}\n{fastkde.coords['var0'].to_numpy()=}\n{fastkde.coords['var1'].to_numpy()}"
+
+    valid_idx = ~np.isnan(fastkde.data[:, pos])  # get indexes, where fastkde compute conditional pdf for x_val
+    pdf = fastkde.data[:, pos][valid_idx]
+    y = fastkde.coords['var1'][valid_idx]
+
+    return _compute_cdf(y, pdf), y
+
+
+def _qet_quantiles_fastkde(fastkde, quantiles: List[float]) -> Tuple[np.ndarray, Dict[float, np.ndarray]]:
+    x = fastkde.coords['var0']
+    x_valid = [not np.all(np.isnan(fastkde.data[:, i])) for i in range(x.shape[0])]
+    x = x[x_valid].to_numpy()
+
+    q_res = {q: np.zeros_like(x, dtype=np.float64) for q in quantiles}
+
+    for i, x_val in enumerate(x):
+        cdf, y_sp = _compute_conditional_cdf_fastkde(x_val, fastkde)
+        for q in quantiles:
+            q_res[q][i] = y_sp[min(bisect_left(cdf, q), len(x))]
+
+    return x, q_res
+
+
 def _gen_proba_bounds(proba: list[float]):
     for p in proba:
-        yield 0.5 + p/2
+        yield 0.5 + p / 2
 
     yield 0.5
 
     for p in reversed(proba):
-        yield 0.5 - p/2
+        yield 0.5 - p / 2
 
 
 def _gen_proba_bound_pairs(proba: list[float]):
@@ -141,6 +170,7 @@ def plot_precision_curve(y_true, y_pred, ax=None, proba: List[float] = None,
                          rug_plot: bool = False,
                          plot_optimal: bool = False,
                          plot_mode: Literal['raw', 'deviation', 'relative'] = 'raw',
+                         strategy: Literal['kde', 'fastkde', 'auto'] = 'auto',
                          x_label: str = 'Actual value', y_label: str = 'Predicted value',
                          show_legend: bool = True,
                          alpha: float = 0.25,
@@ -148,21 +178,30 @@ def plot_precision_curve(y_true, y_pred, ax=None, proba: List[float] = None,
     ax = plt.subplots()[1] if ax is None else ax
     proba = [0.95, 0.5] if proba is None else sorted(proba, reverse=True)
 
-    pdf = scipy.stats.gaussian_kde((y_true, y_pred))
-
     q = [0.5]
     for p in proba:
-        q.append(0.5 - p/2)
-        q.append(0.5 + p/2)
+        q.append(0.5 - p / 2)
+        q.append(0.5 + p / 2)
 
-    x = np.linspace(np.min(y_true), np.max(y_true), 2 ** 8)
-    q_res = _qet_quantiles(
-        x=x,
-        y=(np.min(y_pred), np.max(y_pred)),
-        pdf=pdf,
-        quantiles=q,
-        num_y_dots=2 ** 16
-    )
+    if strategy == 'auto':
+        raise NotImplementedError(f"Strategy {strategy} not implemented")
+
+    if strategy == 'kde':
+        x = np.linspace(np.min(y_true), np.max(y_true), 2 ** 8)
+        pdf = scipy.stats.gaussian_kde((y_true, y_pred))
+        q_res = _qet_quantiles_kde(
+            x=x,
+            y=(np.min(y_pred), np.max(y_pred)),
+            pdf=pdf,
+            quantiles=q,
+            num_y_dots=2 ** 16
+        )
+    else:
+        fastkde = fastKDE.conditional(inputVars=y_pred, conditioningVars=y_true, peak_frac=1e-4)
+        x, q_res = _qet_quantiles_fastkde(
+            fastkde=fastkde,
+            quantiles=q,
+        )
 
     plt_modes = {
         'raw': _raw_plt_mode,
@@ -180,7 +219,7 @@ def plot_precision_curve(y_true, y_pred, ax=None, proba: List[float] = None,
     colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
     for i, (p_u, p_d) in enumerate(_gen_proba_bound_pairs(proba)):
-        c = colors[i if i < len(proba) else 2*len(proba) - i - 1]
+        c = colors[i if i < len(proba) else 2 * len(proba) - i - 1]
         label = f'{(p_u - 0.5) * 2 * 100:.1f}%' if p_u > 0.5 else None
         print(f"p_u: {p_u}, p_d: {p_d}, c: {c}")
         ax.fill_between(x, plt_mode_preprocessor(x, q_res[p_d]), plt_mode_preprocessor(x, q_res[p_u]),
